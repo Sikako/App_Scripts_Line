29188d6e76f212023682dcf0aeeccd0813102458
Sikako <71139205+Sikako@users.noreply.github.com>
2023-05-02 07:58:14 +0800

    Create countColoredCells.js

---
+++ countColoredCells.js
@@ -0,0 +1,22 @@
+function countColoredCells(countRange,colorRef) {
+  var activeRange = SpreadsheetApp.getActiveRange();
+  var activeSheet = activeRange.getSheet();
+  var formula = activeRange.getFormula();
+  
+  var rangeA1Notation = formula.match(/\((.*)\,/).pop();
+  var range = activeSheet.getRange(rangeA1Notation);
+  var bg = range.getBackgrounds();
+  var values = range.getValues();
+  
+  var colorCellA1Notation = formula.match(/\,(.*)\)/).pop();
+  var colorCell = activeSheet.getRange(colorCellA1Notation);
+  var color = colorCell.getBackground();
+  
+  var count = 0;
+  
+  for(var i=0;i<bg.length;i++)
+    for(var j=0;j<bg[0].length;j++)
+      if( bg[i][j] == color )
+        count=count+1;
+  return count;
+};
